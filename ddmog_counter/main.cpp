77cd117e6a374b86f4a15b08312fb674a55dd51c
Zach Wilcher <zwilcher9@gmail.com>
2026-05-21 16:05:57 -0400

    Updated DDMOG Counter to use a simpler faster algorithm

--- ddmog_counter/main.cpp
+++ ddmog_counter/main.cpp
@@ -1,8 +1,7 @@
 #include <cstdint>
-#include <iostream>
 #include <Eigen/Core>
-#include <stack>
 #include <chrono>
+#include <iostream>
 
 int64_t count_ddmogs(const Eigen::VectorXi label_vec, const Eigen::MatrixXi A, const Eigen::Index vertex = 0) {
     int64_t nontrivial_count = 0;
@@ -19,70 +18,47 @@ int64_t count_ddmogs(const Eigen::VectorXi label_vec, const Eigen::MatrixXi A, c
         return 1;
     }
 
-    // compute weight of vertex
-    const int weight = A.row(vertex).dot(label_vec) - label_vec.dot(A.col(vertex));
+    Eigen::VectorXi solution = (A - A.transpose()).row(vertex);
 
-    //
-    // Backtrack to solve sumset problem variant
-    //
+    bool done = false;
+    while(!done) {
 
-    const Eigen::Index l = n - vertex - 1;
-
-    struct StackElement {
-        const Eigen::VectorXi solution;
-        const Eigen::Index index;
-    };
-    std::stack<StackElement> stack;
-
-    stack.push({
-        .solution = Eigen::VectorXi::Zero(l), 
-        .index= l - 1
-    });
-    while(!stack.empty()) {
-        const Eigen::VectorXi cur_solution = stack.top().solution;
-        const Eigen::Index cur_index = stack.top().index;
-        stack.pop();
-        if(cur_index >= 0) {
-            // backtrack for other solutions
-            const Eigen::Index next_index = cur_index - 1;
-            const Eigen::Index label_index = vertex + 1 + cur_index;
-
-            stack.push({
-                .solution = cur_solution, 
-                .index = next_index
-            });
-
-            Eigen::VectorXi attempt2 = cur_solution;
-            attempt2[cur_index] = -label_vec[label_index];
-            stack.push({
-                .solution = attempt2, 
-                .index = next_index
-            });
-
-            Eigen::VectorXi attempt3 = cur_solution;
-            attempt3[cur_index] = label_vec[label_index];
-            stack.push({
-                .solution = attempt3, 
-                .index = next_index
-            });
-
-        } else if (cur_solution.sum() == -weight) {
-            // Found a valid way to connect vertex to other vertices.
-            // Construct new adjacency matrix and recurse.
-            
+        if(solution.dot(label_vec) == 0) {
             Eigen::MatrixXi new_A = A;
-
-
-            for(Eigen::Index i = 0; i < l; i++) {
-                Eigen::Index other_vertex = vertex + 1 + i;
-
-                if(cur_solution[i] > 0) {
+            for(Eigen::Index other_vertex = vertex + 1; other_vertex < n; other_vertex++) {
+                if(solution[other_vertex] > 0) {
                     new_A(vertex, other_vertex) = 1;
-                } else if (cur_solution[i] < 0) {
+                } else if (solution[other_vertex] < 0) {
                     new_A(other_vertex, vertex) = 1;
                 }
             }
             nontrivial_count += count_ddmogs(label_vec, new_A, vertex + 1);
+        }
+
+        // increment
+        bool carry = true;
+        Eigen::Index index = vertex + 1;
+        while(carry && (index < n)) {
+            switch(solution[index]) {
+                case 0:
+                    solution[index] = -1;
+                    carry = false;
+                    break;
+                case -1:
+                    solution[index] = 1;
+                    carry = false;
+                    break;
+                case 1:
+                    solution[index] = 0;
+                    break;
+            }
+            // This is slower than the switch statement above.
+            // solution[index] = ((solution[index] + 3) % 3) - 1;
+            // carry = solution[index] == 0;
+            index++;
+        }
+        if(carry) {
+            done = true;
         }
     }
 
@@ -109,7 +85,7 @@ int main(int argc, char* argv[]) {
     int64_t nontrival_count = count_ddmogs(label_vec, A);
     auto stop = std::chrono::high_resolution_clock::now();
 
-    auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
 
-    std::cout << "Found " << nontrival_count << " DDMOGs with " << n << " vertices in " << duration.count() << " seconds.\n";
+    std::cout << "Found " << nontrival_count << " DDMOGs with " << n << " vertices in " << duration.count() << "ms\n";
 }
